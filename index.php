9082d1ffd993e9a41d11d723c2d27cf595a93744
Nino Burek <nburek@gmail.com>
2020-01-30 14:40:19 +0100

    Just for start

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+
+  
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Title!</title>
+	
+	
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+
+	
+</head>
+
+<body>
+
+<div class="container">
+	<div class="row">
+		<div class="col">
+			
+			<div ng-app="">
+			  <p>Just for Angular test type someting: <input type="text" ng-model="name" class="form-control"></p>
+			  <p ng-bind="name"></p>
+			</div>
+			
+		</div>
+	</div>	
+</div>	
+
+
+	
+	
+</body>
+</html>
